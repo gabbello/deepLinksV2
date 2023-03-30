f92c0ce429d8be5f76cb6ea7d0ef84d9ade9179b
Gabi <gabbello@gmail.com>
2023-03-30 10:08:23 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { createDetailsWidget } from "@livechat/agent-app-sdk";
 
 function App() {
+  const [email, setEmail] = useState(null);
+
+  useEffect(() => {
+    createDetailsWidget({ timeout: 50000 }).then(widget => {
+      // set the initial email
+      const profile = widget.getCustomerProfile();
+      setEmail(profile.email);
+
+      // listen for the customer_profile event and update the email
+      widget.on("customer_profile", profile => {
+        setEmail(profile.email);
+      });
+    });
+  }, []);
+
+  const handleClick = () => {
+    if (email) {
+      const encodedEmail = encodeURIComponent(email);
+      const linkUrl = `https://www.google.com/search?q=${encodedEmail}`;
+      window.open(linkUrl, "_blank");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Search Google</button>
     </div>
   );
 }
