0e72f4420425038ea70c6fc3f1b53a98e263610f
gabbello <gabbello@gmail.com>
2024-06-27 18:21:31 +0300

    Add NS button

--- src/App.js
+++ src/App.js
@@ -35,6 +35,14 @@ function App() {
       window.open(linkUrl, "_blank");
     }
   };
+  
+  const handleNSClick = () => {
+    if (email) {
+      const encodedEmail = encodeURIComponent(email);
+      const linkUrl = `https://backend.nuggetslots.com/customers?page=1&customers[email_cont]=${encodedEmail}`;
+      window.open(linkUrl, "_blank");
+    }
+  };  
 
   return (
     <div className="App" style={{ marginTop: "20px" }}>
@@ -42,6 +50,7 @@ function App() {
         <>
           <button onClick={handleABClick} style={{ marginLeft: "10px", marginRight: "10px" }}>Search email in ArcaneBet BackOffice</button>
           <button onClick={handleCVClick} style={{ marginLeft: "10px", marginRight: "10px", marginTop: "20px"  }}>Search email in CasinoVibes BackOffice</button>
+        <button onClick={handleNSClick} style={{ marginLeft: "10px", marginRight: "10px", marginTop: "20px"  }}>Search email in NuggetSlots BackOffice</button>
         </>
       )}
     </div>
